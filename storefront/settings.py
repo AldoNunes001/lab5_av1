5afb4435a93bb4283e62f67ba2c654d117e35b29
Aldo Nunes <aldonunes001@gmail.com>
2024-10-15 17:37:25 -0300

    cors ok

--- storefront/settings.py
+++ storefront/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'core',
     'graphene_django',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 INTERNAL_IPS = [
     # ...
